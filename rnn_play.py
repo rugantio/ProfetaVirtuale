f3af280dd9cda09fbe0a8e53179093c9eb7d88f9
Rugantio Costa <rugantio@gmail.com>
2018-05-23 01:45:59 +0200

    Add files via upload

---
+++ rnn_play.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import numpy as np
+import my_txtutils
+
+# these must match what was saved !
+ALPHASIZE = my_txtutils.ALPHASIZE
+NLAYERS = 3
+INTERNALSIZE = 512
+
+author = "checkpoints/rnn_train_1526692149-336000000"
+
+ncnt = 0
+with tf.Session() as sess:
+    new_saver = tf.train.import_meta_graph('checkpoints/rnn_train_1526692149-432000000.meta')
+    new_saver.restore(sess, author)
+    x = my_txtutils.convert_from_alphabet(ord("L"))
+    x = np.array([[x]])  # shape [BATCHSIZE, SEQLEN] with BATCHSIZE=1 and SEQLEN=1
+
+    # initial values
+    y = x
+    h = np.zeros([1, INTERNALSIZE * NLAYERS], dtype=np.float32)  # [ BATCHSIZE, INTERNALSIZE * NLAYERS]
+    for i in range(1000000000):
+        yo, h = sess.run(['Yo:0', 'H:0'], feed_dict={'X:0': y, 'pkeep:0': 1., 'Hin:0': h, 'batchsize:0': 1})
+
+        # If sampling is be done from the topn most likely characters, the generated text
+        # is more credible and more "english". If topn is not set, it defaults to the full
+        # distribution (ALPHASIZE)
+
+        # Recommended: topn = 10 for intermediate checkpoints, topn=2 or 3 for fully trained checkpoints
+
+        c = my_txtutils.sample_from_probabilities(yo, topn=2)
+        y = np.array([[c]])  # shape [BATCHSIZE, SEQLEN] with BATCHSIZE=1 and SEQLEN=1
+        c = chr(my_txtutils.convert_to_alphabet(c))
+        print(c, end="")
+
+        if c == '\n':
+            ncnt = 0
+        else:
+            ncnt += 1
+        if ncnt == 100:
+            print("")
+            ncnt = 0
